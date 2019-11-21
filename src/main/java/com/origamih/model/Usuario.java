88f63f2a8763fb0af03fe904152e2c1d53ca31bf
Bruno <brunofmpassos@outlook.com>
2019-11-20 21:23:02 -0300

    Versão 3.0 Retirada da classe de estrutura "Pessoa" (Usuário tem todos os dados em si)

--- src/main/java/com/origamih/model/Usuario.java
+++ src/main/java/com/origamih/model/Usuario.java
@@ -5,7 +5,11 @@ import java.io.Serializable;
 
 @Entity
 @Table(name = "Usuario")
-public class Usuario extends Pessoa implements Serializable{
+public class Usuario implements Serializable{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.TABLE)
+    private Long id;
 
     @ManyToOne
     @JoinColumn(name="user_id")
@@ -23,7 +27,65 @@ public class Usuario extends Pessoa implements Serializable{
     @Transient
     private String cpfpesquisa;
 
+    @Column(nullable=false, length=200)
+    private String nome;
 
+    @Column(nullable=false, length=200)
+    private String cpf;
+
+    @Column(nullable=true, length=200)
+    private String email;
+
+    @Column(nullable=true, length=200)
+    private String instituicao;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+
+    public String getCpf() {
+        return cpf;
+    }
+
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+
+    public String getEmail() {
+        return email;
+    }
+
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
+    public String getInstituicao() {
+        return instituicao;
+    }
+
+
+    public void setInstituicao(String instituicao) {
+        this.instituicao = instituicao;
+    }
 
     public String getPerfil() {
         return perfil;
